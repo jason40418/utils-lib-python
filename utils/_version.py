8b4b02f9a44c926acd85044247b9e2198d8b2252
Jason Lin <jason40418@gmail.com>
2021-08-15 23:03:54 +0800

    [Update] Regular the version naming
    Signed-off-by: Jason Lin <jason40418@gmail.com>

--- utils/_version.py
+++ utils/_version.py
@@ -1,5 +1,49 @@
+MILESTONE_LIST = {
+    "develope"            : "dev",
+    "pre-alpha"           : "pa",
+    "alpha"               : "a",
+    "beta"                : "b",
+    "preview"             : "p",
+    "release-candidate"   : "rc",
+    "general-availability": "ga",
+    "release"             : ""
+}
 
-MILESTONE = "dev"
+MILESTONE    = "develope"
+MAJOR        = "0"
+MINOR        = "1"
+REVISION     = "0"
+YEAR         = "2021"
+MONTH        = "08"
+DAY          = "15"
+BUILD        = "00"
 
+"""
+Version naming rule:
+{major}.{minor}.{revision}({milestone}{build})
+
+build naming rule: YYYYMMDDBB
+YYYY : Year
+MM   : Month
+DD   : Day
+BB   : Build time
+"""
 def get_version ():
-    return '0.1.0.277002dev'
+    semantic  = ".".join([MAJOR, MINOR, REVISION])
+    milestone = MILESTONE_LIST[MILESTONE]
+
+    if MILESTONE == 'develope':
+        build = "".join([YEAR, MONTH, DAY, BUILD])
+    elif MILESTONE == 'release':
+        build = ""
+    else:
+        build = BUILD
+
+    version   = "".join([
+        semantic,
+        "_" if MILESTONE != "release" else "",
+        milestone,
+        build,
+        "" if MILESTONE == "develope" else ""
+    ])
+    return version

d4f82768a2f9cf3595d70b821ccabae1d08cbe89
Jason Lin <jason40418@gmail.com>
2021-08-21 14:30:30 +0800

    [TestCase] Add Covert/CovertString testcase
    Signed-off-by: Jason Lin <jason40418@gmail.com>

---
+++ tests/convert/convert_string_test.py
@@ -0,0 +1,22 @@
+from utils.convert.convert_string import ConvertString
+
+
+def test_empty_string():
+    test_list = list()
+    cs = ConvertString()
+    result = cs.list_to_string(test_list, "|")
+    assert result == ""
+
+
+def test_default_seperator():
+    test_list = [10, "eng", "0"]
+    cs = ConvertString()
+    result = cs.list_to_string(test_list)
+    assert result == "10|eng|0"
+
+
+def test_comma_seperator():
+    test_list = [10, "eng", "0"]
+    cs = ConvertString()
+    result = cs.list_to_string(test_list, ",")
+    assert result == "10,eng,0"

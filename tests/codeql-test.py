068f987099a1a61b9493238390e677ed8b914a8c
Jason Lin <jason40418@gmail.com>
2022-10-07 23:26:43 +0800

    [Test] Test codeql functionality
    Signed-off-by: Jason Lin <jason40418@gmail.com>

---
+++ tests/codeql-test.py
@@ -0,0 +1,20 @@
+import argparse
+import os
+
+
+def open_path(file_path):
+    file = open(file_path, "r")
+
+
+if "__name__" == "__main__":
+    parser = argparse.ArgumentParser (
+        conflict_handler = 'resolve',
+        fromfile_prefix_chars = '@'
+        )
+
+
+    parser.add_argument ("-file", "--file", dest = 'file',
+                         help = "JSON configuration file for multiple payloads and embedded drivers.")
+
+    args = parser.parse_args()
+    open_path (args.file)
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,14 @@ from . import _version
 name = 'utils-lib'
 
 __VERSION__ = _version.get_version()
+
+data = 'def'
+
+if data == 'abc':
+    while True:
+        print ('Infinite loop')
+
+def area(r):
+    #if DEBUG:
+    #   print("Computing area of %r" % r)
+    return r.length * r.width
